757320e02690c7e93dc092fde6bc1017307029a2
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-03-25 23:21:11 -0300

    Projeto de exemplo da aula sobre entrada de data e hora com Calendar

---
+++ Calendar/src/main/java/com/algaworks/cursojavaee/PerfilUsuarioBean.java
@@ -0,0 +1,46 @@
+package com.algaworks.cursojavaee;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private Date dataNascimento;
+
+	public void atualizar() {
+		System.out.println("Data nascimento: " + this.dataNascimento);
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado!"));
+	}
+	
+	public Date getDataHoje() {
+		return new Date();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
