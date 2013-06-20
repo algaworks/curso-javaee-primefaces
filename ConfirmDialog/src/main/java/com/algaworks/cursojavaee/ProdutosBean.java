436e4bd1e7692f59902bf7fbc22d009b7568b67d
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-06-20 01:06:14 -0300

    Diálogo de confirmação com ConfirmDialog

---
+++ ConfirmDialog/src/main/java/com/algaworks/cursojavaee/ProdutosBean.java
@@ -0,0 +1,42 @@
+package com.algaworks.cursojavaee;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class ProdutosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<String> produtos = new ArrayList<>();
+	private String produtoSelecionado;
+
+	public ProdutosBean() {
+		produtos.add("Arroz");
+		produtos.add("Feijão");
+		produtos.add("Queijo");
+		produtos.add("Notebook");
+	}
+
+	public void excluirProduto() {
+		produtos.remove(produtoSelecionado);
+	}
+	
+	public List<String> getProdutos() {
+		return produtos;
+	}
+
+	public String getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(String produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+	
+}
