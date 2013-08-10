bb21b2a22a807b4c00cc4e6731c4bcb460c0cf5c
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-08-09 21:58:50 -0300

    Combo boxes dependentes de categorias e subcategorias

---
+++ ComboBoxesDependentesCategorias/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -0,0 +1,71 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+
+import com.algaworks.pedidovenda.model.Categoria;
+import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.repository.Categorias;
+import com.algaworks.pedidovenda.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class CadastroProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Categorias categorias;
+	
+	private Produto produto;
+	private Categoria categoriaPai;
+	
+	private List<Categoria> categoriasRaizes;
+	private List<Categoria> subcategorias;
+	
+	public CadastroProdutoBean() {
+		produto = new Produto();
+	}
+	
+	public void inicializar() {
+		if (FacesUtil.isNotPostback()) {
+			categoriasRaizes = categorias.raizes();
+		}
+	}
+	
+	public void carregarSubcategorias() {
+		subcategorias = categorias.subcategoriasDe(categoriaPai);
+	}
+	
+	public void salvar() {
+		System.out.println("Categoria pai selecionada: " + categoriaPai.getDescricao());
+		System.out.println("Subcategoria selecionada: " + produto.getCategoria().getDescricao());
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	@NotNull
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
+	}
+
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
+	}
+
+}
---
+++ ComboBoxesDependentesCategorias/src/main/java/com/algaworks/pedidovenda/util/jsf/FacesUtil.java
@@ -0,0 +1,21 @@
+package com.algaworks.pedidovenda.util.jsf;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class FacesUtil {
+
+	public static boolean isPostback() {
+		return FacesContext.getCurrentInstance().isPostback();
+	}
+	
+	public static boolean isNotPostback() {
+		return !isPostback();
+	}
+	
+	public static void addErrorMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+	}
+	
+}
