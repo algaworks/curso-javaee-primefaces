aeebca55c1663cfe02c3fe8e0b2ad02136c67607
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-08-17 23:07:10 -0300

    Implementando o cadastro de pedidos sem itens

---
+++ ImplementandoCadastroPedidosSemItens/src/main/java/com/algaworks/pedidovenda/controller/CadastroPedidoBean.java
@@ -0,0 +1,75 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.pedidovenda.model.Cliente;
+import com.algaworks.pedidovenda.model.EnderecoEntrega;
+import com.algaworks.pedidovenda.model.FormaPagamento;
+import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.model.Usuario;
+import com.algaworks.pedidovenda.repository.Clientes;
+import com.algaworks.pedidovenda.repository.Usuarios;
+import com.algaworks.pedidovenda.service.CadastroPedidoService;
+import com.algaworks.pedidovenda.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class CadastroPedidoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Usuarios usuarios;
+	
+	@Inject
+	private Clientes clientes;
+	
+	@Inject
+	private CadastroPedidoService cadastroPedidoService;
+	
+	private Pedido pedido;
+	private List<Usuario> vendedores;
+	
+	public CadastroPedidoBean() {
+		limpar();
+	}
+	
+	public void inicializar() {
+		if (FacesUtil.isNotPostback()) {
+			this.vendedores = this.usuarios.vendedores();
+		}
+	}
+	
+	private void limpar() {
+		pedido = new Pedido();
+		pedido.setEnderecoEntrega(new EnderecoEntrega());
+	}
+	
+	public void salvar() {
+		this.pedido = this.cadastroPedidoService.salvar(this.pedido);
+		
+		FacesUtil.addInfoMessage("Pedido salvo com sucesso!");
+	}
+	
+	public FormaPagamento[] getFormasPagamento() {
+		return FormaPagamento.values();
+	}
+	
+	public List<Cliente> completarCliente(String nome) {
+		return this.clientes.porNome(nome);
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public List<Usuario> getVendedores() {
+		return vendedores;
+	}
+
+}
