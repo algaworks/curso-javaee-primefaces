57aee6953569a894db031196eda52f3fe60e2f4b
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-08-24 15:39:24 -0300

    Adicionando itens ao pedido

---
+++ AdicionandoItensPedido/src/main/java/com/algaworks/pedidovenda/repository/Usuarios.java
@@ -0,0 +1,28 @@
+package com.algaworks.pedidovenda.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.algaworks.pedidovenda.model.Usuario;
+
+public class Usuarios implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+	
+	public Usuario porId(Long id) {
+		return this.manager.find(Usuario.class, id);
+	}
+	
+	public List<Usuario> vendedores() {
+		// TODO filtrar apenas vendedores (por um grupo específico)
+		return this.manager.createQuery("from Usuario", Usuario.class)
+				.getResultList();
+	}
+	
+}
