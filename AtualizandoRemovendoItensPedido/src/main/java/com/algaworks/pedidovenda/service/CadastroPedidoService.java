062f19bdb6ddda017d6787fcd282d4ebb870c0b6
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-08-24 16:16:12 -0300

    Atualizando e removendo itens do pedido

---
+++ AtualizandoRemovendoItensPedido/src/main/java/com/algaworks/pedidovenda/service/CadastroPedidoService.java
@@ -0,0 +1,31 @@
+package com.algaworks.pedidovenda.service;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.model.StatusPedido;
+import com.algaworks.pedidovenda.repository.Pedidos;
+import com.algaworks.pedidovenda.util.jpa.Transactional;
+
+public class CadastroPedidoService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Pedidos pedidos;
+	
+	@Transactional
+	public Pedido salvar(Pedido pedido) {
+		if (pedido.isNovo()) {
+			pedido.setDataCriacao(new Date());
+			pedido.setStatus(StatusPedido.ORCAMENTO);
+		}
+		
+		pedido = this.pedidos.guardar(pedido);
+		return pedido;
+	}
+	
+}
---
+++ AtualizandoRemovendoItensPedido/src/main/java/com/algaworks/pedidovenda/util/AppContextListener.java
@@ -0,0 +1,19 @@
+package com.algaworks.pedidovenda.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		System.setProperty("org.apache.el.parser.COERCE_TO_ZERO", "false");
+	}
+
+}
