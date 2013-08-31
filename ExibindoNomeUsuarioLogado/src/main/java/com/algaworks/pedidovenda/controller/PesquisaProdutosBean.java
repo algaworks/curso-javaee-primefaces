ad05d1b6f87440b5e9234c0232b4ed9a14e10984
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-08-31 14:34:14 -0300

    Exibindo o nome do usuário logado

---
+++ ExibindoNomeUsuarioLogado/src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -0,0 +1,61 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.repository.Produtos;
+import com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
+import com.algaworks.pedidovenda.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class PesquisaProdutosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Produtos produtos;
+	
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
+	
+	private Produto produtoSelecionado;
+	
+	public PesquisaProdutosBean() {
+		filtro = new ProdutoFilter();
+	}
+	
+	public void excluir() {
+		produtos.remover(produtoSelecionado);
+		produtosFiltrados.remove(produtoSelecionado);
+		
+		FacesUtil.addInfoMessage("Produto " + produtoSelecionado.getSku() 
+				+ " excluído com sucesso.");
+	}
+	
+	public void pesquisar() {
+		produtosFiltrados = produtos.filtrados(filtro);
+	}
+	
+	public List<Produto> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
+
+	public ProdutoFilter getFiltro() {
+		return filtro;
+	}
+
+	public Produto getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(Produto produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+	
+}
