42c3f57f339340577566bdfa25824ad05e918e85
Thiago Faria de Andrade <thiagofa@gmail.com>
2015-02-06 19:09:24 -0200

    Aula sobre customizacao de mensagens de validacao client-side

---
+++ CustomizandoMensagensValidacaoClientSide/src/main/webapp/resources/locales.js
@@ -0,0 +1,6 @@
+PrimeFaces.locales['pt_BR'] = {
+	messages : {
+		'javax.faces.component.UIInput.REQUIRED' : '{0} é obrigatório',
+		'javax.faces.validator.LengthValidator.MAXIMUM' : '{1} deve ter no máximo {0} caracteres'
+	}
+}
