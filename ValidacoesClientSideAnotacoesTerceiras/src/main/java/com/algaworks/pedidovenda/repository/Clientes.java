a409560069608ac1914ff5a23e01778b81ccda1f
Thiago Faria de Andrade <thiagofa@gmail.com>
2015-02-09 21:40:57 -0200

    Aula sobre validações client-side com anotações terceiras

---
+++ ValidacoesClientSideAnotacoesTerceiras/src/main/java/com/algaworks/pedidovenda/repository/Clientes.java
@@ -0,0 +1,29 @@
+package com.algaworks.pedidovenda.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.algaworks.pedidovenda.model.Cliente;
+
+public class Clientes implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+	
+	public Cliente porId(Long id) {
+		return this.manager.find(Cliente.class, id);
+	}
+	
+	public List<Cliente> porNome(String nome) {
+		return this.manager.createQuery("from Cliente " +
+				"where upper(nome) like :nome", Cliente.class)
+				.setParameter("nome", nome.toUpperCase() + "%")
+				.getResultList();
+	}
+	
+}
---
+++ ValidacoesClientSideAnotacoesTerceiras/src/main/webapp/resources/js/validadores.js
@@ -0,0 +1,54 @@
+PrimeFaces.locales['pt_BR'] = {
+	messages : {
+		'org.hibernate.validator.constraints.NotBlank.message' : '{0} não pode estar em branco'
+	}
+}
+
+PrimeFaces.validator.NotBlank = {
+	
+	MESSAGE_ID : 'org.hibernate.validator.constraints.NotBlank.message',
+		
+	validate : function(element, value) {
+		if (value === null || value === undefined || value.trim() === '') {
+			var msg = element.data('msg-notblank');
+			var label = element.data('p-label');
+			var context = PrimeFaces.util.ValidationContext;
+			var msgObj;
+			
+			if (!msg) {
+				msgObj = context.getMessage(this.MESSAGE_ID, label);
+			} else {
+				var msgObj = {
+					summary : msg,
+					detail : msg
+				}
+			}
+			
+			throw msgObj;
+		}
+	}
+		
+};
+
+PrimeFaces.validator.SKU = {
+	
+	pattern : /^([a-zA-Z]{2}\d{4,18})?$/,
+		
+	validate : function(element, value) {
+		if (!this.pattern.test(value)) {
+			var msg = element.data('msg-sku');
+			
+			if (!msg) {
+				msg = 'SKU não é válido.';
+			}
+			
+			var msgObj = {
+				summary : msg,
+				detail : msg
+			}
+			
+			throw msgObj;
+		}
+	}
+		
+};
