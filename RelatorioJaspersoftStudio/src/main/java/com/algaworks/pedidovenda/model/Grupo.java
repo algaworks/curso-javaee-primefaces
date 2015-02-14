482102f942b6a1ae30e20cb2fa08cb9c191800f0
Thiago Faria de Andrade <thiagofa@gmail.com>
2015-02-14 14:32:26 -0200

    Relatorio com Jaspersoft Studio

---
+++ RelatorioJaspersoftStudio/src/main/java/com/algaworks/pedidovenda/model/Grupo.java
@@ -0,0 +1,74 @@
+package com.algaworks.pedidovenda.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "grupo")
+public class Grupo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private String descricao;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(nullable=false, length=40)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Column(nullable=false, length=80)
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grupo other = (Grupo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
